755df660279dc9d2fe8138df5ec95c0dc1b148d3
Timothy <18tfl1@queensu.ca>
2021-02-15 02:19:52 -0500

    edited post request information

--- src/Methods/Auth.js
+++ src/Methods/Auth.js
@@ -3,11 +3,14 @@ const axios = require('axios');
 const Auth=(username, password)=>{
     
     const postData = {
-        userName: username,
-        password: password
+        'userName': username,
+        'password': password
     }
+
     //for some reason it only works for http but not https
-    fetch("https://lowlights.lol/api/login", {method: 'POST', headers:postData}).then(function(data){
+    fetch("https://lowlights.lol/api/login", {method: 'POST', headers:{
+        "Content-Type": "application/x-www-form-urlencoded", 
+    }, body: new URLSearchParams(postData)}).then(function(data){
         console.log(data)
     }).catch(function(err){
         console.log(err)
